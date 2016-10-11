8175f305bca09e7672f8ecb176f562969efed202
lfdeiby <lf.deiby@gmail.com>
2016-10-10 22:07:13 -0400

    Probando set, update, lista en Firebase

--- app.js
+++ app.js
@@ -26,6 +26,72 @@ app.post('/token-device', function(req, res){
 	res.send(req.body.token);
 });
 
+app.get('/firebase/set', function(req, res){
+	var db = firebase.database();
+	var ref = db.ref('server/saving-data/fireblog');
+	var usersRef = ref.child('users');
+	//CON ESTE METODO CREAMOS AMBOS OBJETOS 
+	usersRef.set({
+		alanisawesome:{
+			date_of_birth: "June 23, 1912",
+			full_name: "Alan Turing"
+		},
+		gracehop:{
+			date_of_birth: "December 9, 1906",
+			full_name: "Grace Hopper"
+		}
+	});
+	//CREAR UN SOLO OBJETO
+	usersRef.child('deiby').set({
+		date_of_birth: "August 3, 1988",
+		full_name: "Deiby Loayza"
+	});
+	//La diferencia es que en el primer metodo ejecuto una consulta y si lo hiciera
+	//con la segunda opción ejecutaría varias consultas
+	res.send("OK");
+});
+app.get('/firebase/update', function(req, res){
+	var db = firebase.database();
+	var ref = db.ref('server/saving-data/fireblog');
+	var usersRef = ref.child('users');
+	//ACTULIZAR LOS CAMPOS DE UN OBJETO
+	var yoRef = usersRef.child('deiby');
+	yoRef.update({
+		nickname: "Amazing YO",
+		date_of_birth: "August 1, 1988"
+	});
+	//ACTUALIZAR CAMPOS DE VARIOS OBJETOS
+	usersRef.update({
+		'alanisawesome/nickname': "ralf",
+		'gracehop/nickname': "Grace anatomy"
+	},function(err){
+		if(err){
+			res.send(err);
+		}else{
+			res.send("Modifico Correctamente");
+		}
+	});
+});
+app.get('/firebase/lista', function(req, res){
+	var db = firebase.database();
+	var ref = db.ref('server/saving-data/fireblog');
+	var postsRef = ref.child('posts');
+	//SI DEFINIMOS COMO PUSH CADA VEZ QUE UTILICEMOS EL METODO "set" CREARÁ UN ID
+	//UNICO E IRA ALMACENANDO DE ACRUDO AL ID QUE NO SE REPITE
+	var newPostRef = postsRef.push();
+	newPostRef.set({
+		author: "gracehop",
+		title: "Annoucing COBOL, a new console log"
+	});
+	//OTRA MANERA DE AGREGAR A LA LISTA
+	postsRef.push().set({
+		author: "deiby",
+		title: "The Turing Machine 2"
+	});
+
+	res.send("OK: " + newPostRef.key);
+});
+
 var v1 = require('./v1');
 app.use('/v1', v1);
 
